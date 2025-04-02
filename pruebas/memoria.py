25d18d3de20dc6699332ee13e61f7f0e26d9da87
claudio <claudiio.claudiio@icloud.com>
2025-04-02 16:43:11 +0200

    ignorando carpeta pruebas_codigo

--- pruebas/memoria.py
+++
@@ -1,8 +0,0 @@
-from langchain.memory import ConversationBufferMemory
-
-memory = ConversationBufferMemory(return_messages=True)
-mensajes = [[{"input": "hi"}, {"output": "whats up"}], [{"input": "nice"}, {"output": "okey"}]]
-
-memory.save_context([[{"input": "hi"}, {"output": "whats up"}], [{"input": "nice"}, {"output": "okey"}]])
-
-print(memory.load_memory_variables({}))
